5ffbc24beb86149126d826d10f66d2b0e19b53d0
kaihatsutesutoyou <122243922+kaihatsutesutoyou@users.noreply.github.com>
2023-01-10 17:53:55 +0900

    1 file modified, 1 added

--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -3,7 +3,11 @@
     <NuxtLink to="/">Home2</NuxtLink> |
     <NuxtLink to="/about">About2</NuxtLink> |
     <NuxtLink to="/sample">sample</NuxtLink> |
-    <NuxtLink to="/users/list">userlist</NuxtLink> |
+    <NuxtLink :to="{ name: 'index' }">index</NuxtLink> |
+    <!-- <NuxtLink :to="{ name: 'users-index' }">user index</NuxtLink> | -->
+    <NuxtLink to="/users/">user index</NuxtLink> |
+    <NuxtLink :to="{ name: 'users-list' }">user list</NuxtLink> |
+    <NuxtLink to="/users/list">user list2</NuxtLink> |
     <NuxtLink to="https://google.com" target="_blank">google</NuxtLink> |
     <NuxtLink></NuxtLink>
   </nav>
